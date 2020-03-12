7543fe22aa06c37299728047bd1dd9678ab78ab6
Aniket Kumar <44335648+Aniketkjnv@users.noreply.github.com>
2020-03-12 12:04:02 +0530

    Add files via upload

---
+++ Char_inc.java
@@ -0,0 +1,15 @@
+// A program of character increment	
+// date : 12-03-2020
+
+public class Char_inc
+{
+	public static void main(String args[])
+	{
+	char a='C';
+	char b=85;
+	a++;
+	b++;
+	
+	System.out.println(a+" "+b);
+	}
+}
